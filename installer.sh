d54a49292cb15644c8587937bfe45e1bc93096be
Abderrahim <bahimzabir@gmail.com>
2023-08-15 22:25:45 +0100

    some colors added

--- installer.sh
+++ installer.sh
@@ -12,7 +12,7 @@ default_shell=$(basename "$SHELL")
 if [ "$default_shell" = "bash" ] || [ "$default_shell" = "zsh" ]; then
     echo 'alias xclean="/bin/bash $HOME/xclean.sh"' >> "$HOME/.bashrc"
     echo 'alias xclean="/bin/bash $HOME/xclean.sh"' >> "$HOME/.zshrc"
-    echo -e "\033[4;1;34\\n\nAlias 'xclean' has been added to your shell configuration."
+    echo -e "\033[K\033[32m\n\nAlias 'xclean' has been added to your shell configuration."
 else
     echo "Unknown shell '$default_shell'. You might need to add the alias manually to your shell configuration."
 fi
